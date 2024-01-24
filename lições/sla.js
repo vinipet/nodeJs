1d42a51e67a8520b6085525953ad384aac8034b4
Vinicius <79338898+vinipet@users.noreply.github.com>
2024-01-24 09:44:25 -0300

    nodemon install

---
+++ lições/sla.js
@@ -0,0 +1,17 @@
+const Express = require('express')
+const app = Express()
+
+app.get("/",function(req,res){
+   res.send("seja bem vindo porra")
+})
+app.get("/sobre",function(req,res){
+   res.send("sobre")
+})
+
+app.get('/ola/:nome/:cargo', function(req,res){
+   res.send(`Ola ${req.params.nome}. Você esta atuando como ${req.params.cargo}!`)
+})
+
+app.listen(8081, function(){
+   console.log("servidor rodando!!!")
+})
